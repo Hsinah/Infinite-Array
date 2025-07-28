d4e53b6510080303a37f9dfec8838e6e2593c5b0
Hanish T <156298019+Hsinah@users.noreply.github.com>
2025-07-28 19:20:47 +0530

    Add files via upload

---
+++ InfiniteArray.java
@@ -0,0 +1,38 @@
+
+
+//https://www.geeksforgeeks.org/dsa/find-position-element-sorted-array-infinite-numbers/
+public class InfiniteArray
+{
+    public static int ans(int arr[],int target)
+    {
+        int start =0;
+        int end =1;
+        while(target>arr[end])
+        {
+            int temp=end+1;
+            end = end+(end-start+1)*2;
+            start = temp;
+        }
+        return binarysearch(arr,target,start,end);
+    }
+    public static int binarysearch(int arr[],int target,int start ,int end)
+    {
+        while(start<=end)
+        {
+            int mid =start+(end-start)/2;
+            if(target<arr[mid])
+                end=mid-1;
+            else if(target>arr[mid])
+                start=mid+1;
+            else if(target==arr[mid])
+                return mid;
+        }
+        return -1;
+    }
+    public static void main(String args[])
+    {
+        int[] arr = {3, 5, 7, 9, 10, 90,100, 130, 140, 160, 170};
+        int target = 10;
+        System.out.println(ans(arr,target));
+    }
+}
